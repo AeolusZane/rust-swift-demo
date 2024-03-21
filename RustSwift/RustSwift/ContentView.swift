20afccddeafd9975f4eed49a97db8b4a0f4d09ce
AeolusZane <1393649219@qq.com>
2024-03-21 12:01:34 +0800

    rust project

---
+++ RustSwift/RustSwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RustSwift
+//
+//  Created by 张敬峥 on 2024/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RustSwift/RustSwift/RustSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  RustSwiftApp.swift
+//  RustSwift
+//
+//  Created by 张敬峥 on 2024/3/21.
+//
+
+import SwiftUI
+
+@main
+struct RustSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
